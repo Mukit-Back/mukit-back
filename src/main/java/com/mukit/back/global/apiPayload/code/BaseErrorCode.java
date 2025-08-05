70d152d3dc3105369563c97e72204d23303f3d0a
김준환 <musoyou1085@gmail.com>
2025-08-05 22:45:57 +0900

    :rocket: chore: 에러 핸들러, 응답 통일 설정

---
+++ src/main/java/com/mukit/back/global/apiPayload/code/BaseErrorCode.java
@@ -0,0 +1,15 @@
+package com.mukit.back.global.apiPayload.code;
+
+import com.mukit.back.global.apiPayload.CustomResponse;
+import org.springframework.http.HttpStatus;
+
+public interface BaseErrorCode {
+
+    HttpStatus getHttpStatus();
+    String getCode();
+    String getMessage();
+
+    default CustomResponse<Void> getErrorResponse() {
+        return CustomResponse.onFailure(getCode(), getMessage());
+    }
+}
