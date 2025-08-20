c406847c2b60dd9b80b8b6708f697164869f2f2f
김준환 <musoyou1085@gmail.com>
2025-08-20 15:32:19 +0900

    🚀 Chore: url 추가

--- src/main/java/com/mukit/back/global/config/WebConfig.java
+++ src/main/java/com/mukit/back/global/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://127.0.0.1:5500", "http://localhost:5500")
+                .allowedOrigins("http://127.0.0.1:5500", "http://localhost:5500", "https://api.withtime.shop")
                 .allowedMethods("GET","POST","PUT","PATCH","DELETE","OPTIONS")
                 .allowedHeaders("*")
                 .maxAge(3600);
