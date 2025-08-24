b8e5dbb42b71f1cf6c217f84ac1867d0c39cf7f7
김준환 <musoyou1085@gmail.com>
2025-08-24 21:51:32 +0900

    :rocket: chore: market 추가 url 변경

--- src/main/java/com/mukit/back/domain/market/controller/MarketController.java
+++ src/main/java/com/mukit/back/domain/market/controller/MarketController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/market")
+@RequestMapping("/api/markets")
 @RequiredArgsConstructor
 @Tag(name = "Market", description = "마켓")
 public class MarketController {
